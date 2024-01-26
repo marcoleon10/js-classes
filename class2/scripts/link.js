70387b40550cf9d35a6853f022dc9695fa399281
marcoleon10 <marco.leon11@uabc.edu.mx>
2024-01-25 17:27:49 -0800

    add link to server

--- class2/scripts/link.js
+++ class2/scripts/link.js
@@ -2,6 +2,29 @@
 let iconImportant = false;
 function saveTask() {
   console.log("Saving task");
+  //get the values 
+  const title = $("#title").val();
+  const description = $("#description").val();
+  const color = $("#color").val();
+  const startDate = $("#startDate").val();
+  const status = $("#status").val();
+  const budget = $("#budget").val();
+  //build the object
+  let taskToSave = new Task(title, description, color, startDate, status, budget);
+  console.log(taskToSave);
+//save logic
+$.ajax({
+  type: "POST",
+  url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+  data: JSON.stringify(taskToSave),
+  contentType: "application/json",
+  success: function(res){
+    console.log(res);
+  },
+  error: function(error) {
+  console.log(error);
+  }
+})
 }
 
 function changeIcon() {
@@ -17,6 +40,32 @@ function changeIcon() {
   }
   //please try to restore the icon
 }
+
+function testRequest(){
+  $.ajax({
+    type: "GET",
+    url: "http://fsdiapi.azurewebsites.net"
+  });
+
+  displayTask(taskToSave);
+}
+
+function displayTask(task){
+  let syntax = `<div class='task'>
+<div class='info'>
+<h5>${task.title}</h5>
+<p>${task.description}</p>
+</div>
+<label>${task.status}</label>
+<div class='date-budget'>
+<label>${task.startDate}</label>
+<label>${task.budget}</label>
+</div>
+</div>`;
+
+$("#sectionA").append(syntax);
+}
+//Try to read all the elements in the inputs
 
 function init() {
   console.log("This is the parent of everything");
